12ace8dac0c5891a4fa48d5bdbb96a3da39ed7bb
decieun <decieun@gmail.com>
2024-07-16 16:40:38 +0900

    init

---
+++ src/test/java/com/whisky/dramdiary/DramdiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whisky.dramdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DramdiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
